91d089c178af845c0f2a35af071e6b602d7ef872
BrianG22 <moso2201@gmail.com>
2025-11-20 13:22:48 -0700

    css

---
+++ src/Views/home/about.php
@@ -0,0 +1,71 @@
+
+<!DOCTYPE html>
+<html lang="es">
+
+
+
+<body>
+   
+
+    <section class="banner">
+        <div class="container banner-content">
+            <div class="banner-text">
+                <h1>Acerca de Nosotros</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+            </div>
+            <div class="banner-image">
+                <img src="assets/img/about-hero.svg" alt="About Hero">
+            </div>
+        </div>
+    </section>
+
+    <section class="services-section">
+        <div class="container">
+            <div class="section-header">
+                <h1>Nuestros Servicios</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+            </div>
+            <div class="services-grid">
+                <div class="service-item">
+                    <div class="service-icon"><i class="fa fa-truck fa-lg"></i></div>
+                    <h2>Envios</h2>
+                </div>
+                <div class="service-item">
+                    <div class="service-icon"><i class="fas fa-exchange-alt"></i></div>
+                    <h2>Devoluciones</h2>
+                </div>
+                <div class="service-item">
+                    <div class="service-icon"><i class="fa fa-percent"></i></div>
+                    <h2>Promociones</h2>
+                </div>
+                <div class="service-item">
+                    <div class="service-icon"><i class="fa fa-user"></i></div>
+                    <h2>Servicio las 24h</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="brands-section">
+        <div class="container">
+            <div class="section-header">
+                <h1>Marcas</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+            </div>
+            <div class="brands-grid">
+                <a href="#"><img class="brand-img" src="assets/img/LOGOS/Ryzen.png" alt="Brand Logo"></a>
+                <a href="#"><img class="brand-img" src="assets/img/LOGOS/INTEL.png" alt="Brand Logo"></a>
+                <a href="#"><img class="brand-img" src="assets/img/LOGOS/NVIDIA.png" alt="Brand Logo"></a>
+                <a href="#"><img class="brand-img" src="assets/img/LOGOS/DELL.png" alt="Brand Logo"></a>
+            </div>
+        </div>
+    </section>
+
+  
+
+</body>
+</html>
